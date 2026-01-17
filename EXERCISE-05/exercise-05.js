e95cde3e9d35468c74558a33a13fd8851ae1121f
ahmedkaal <yaaska1943@gmail.coml>
2026-01-17 12:59:05 -0800

    Exercise-05 Submitted

---
+++ EXERCISE-05/exercise-05.js
@@ -0,0 +1,15 @@
+
+// CSS FOR STYLING ONLY
+let css1 = `font-size:1rem ; color:blue ; font-weight:bold; padding:0.5rem 1rem; background:yellow`
+let css2 = "background:green; color:#fff"
+
+
+// EXERCISE 05 ARROW FUNCTION
+console.log(`%cEXERCISE 05 ARROW FUNCTION `, css1)
+
+const add=(num1,num2)=>{
+    return num1+num2;
+}
+console.log(`The sum of Num1 and Num2 is ${add(4,5)}`)
+console.log(`The sum of Num1 and Num2 is ${add(10,20)}`)
+
