c97ec265910b02519bf5b6250bac7c44a4a1328f
ahmedkaal <yaaska1943@gmail.coml>
2026-01-18 07:40:22 -0800

    Exercise-06 submitted and readme file modified

---
+++ EXERCISE-06/exercise-06.js
@@ -0,0 +1,16 @@
+
+// CSS FOR STYLING ONLY
+let css1 = `font-size:1rem ; color:blue ; font-weight:bold; padding:0.5rem 1rem; background:yellow`
+let red = `background:red; color:#fff`
+let green = "background:red; color:#fff"
+let blue = "background:blue; color:#fff"
+
+
+// EXERCISE 06 ARRAYS
+console.log(`%cEXERCISE 06 ARRAYS `, css1)
+
+let colors=["red","green",'blue']
+
+console.log(`%cThe first color is ${colors[0]}`,red)
+console.log(`%cThe second color is ${colors[1]}`,green)
+console.log(`%cThe third color is ${colors[2]}`,blue)
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ ___
 * Exercise-1 Arithmetic operators   => DONE
 * Exercise-2 Comparison operators   => DONE
 * Exercise-3 Logical operators      => DONE
-* Exercise-4 Functions Dec/Express. => DONE
+* Exercise-4 Functions Dec/Express  => DONE
+* Exercise-5 ARROW FUCCTION         => DONE
+* Exercise-6 ARRAYS                 => DONE
