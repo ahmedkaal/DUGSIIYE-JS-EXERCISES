a2ecb6da71413e315a50abb0d8116f140c70bda3
ahmedkaal <yaaska1943@gmail.coml>
2026-01-26 12:04:31 -0800

    Exercise-19 submitted and ReadMe file modified

---
+++ EXERCISE-19/exercise-19.js
@@ -0,0 +1,29 @@
+
+
+
+
+// CSS FOR STYLING ONLY
+let css1 = `font-size:1rem ; color:blue ; font-weight:bold; padding:0.5rem 1rem; background:yellow`
+let red = `background:blue; color:#fff ; padding:0.5rem 1rem;`
+let green = `background:green; color:#fff`
+
+
+// EXERCISE 19 FOR EACH METHOD
+console.log(`%cEXERCISE 19 FOR EACH METHOD `, css1)
+// ..........................................
+
+console.log('%cFOR EACH METHOD', red);
+
+let numbers= [1,2,3,4,5]
+
+numbers.forEach((number)=>{
+      console.log(`  number ${number}`);
+      
+})
+
+
+
+
+
+
+
