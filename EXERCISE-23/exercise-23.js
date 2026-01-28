9a6a404aed2895302d7ac61f5fff50c841c2ac59
ahmedkaal <yaaska1943@gmail.coml>
2026-01-28 11:23:46 -0800

    some change made

---
+++ EXERCISE-23/exercise-23.js
@@ -0,0 +1,22 @@
+
+
+
+
+// CSS FOR STYLING ONLY
+let css1 = `font-size:1rem ; color:blue ; font-weight:bold; padding:0.5rem 1rem; background:yellow`
+let red = `background:blue; color:#fff ; padding:0.5rem 1rem;`
+let green = `background:green; color:#fff`
+
+
+// EXERCISE 23 DESTRUCTURING OBJECTS
+console.log(`%cEXERCISE 23 DESTRUCTURING OBJECTS `, css1)
+console.log(`%cuse object destructuring to extract make and model properties from the object {make:"Toyota" , model:"Corolla" , year:2020} `, red)
+
+let car={
+    make:"Toyota",
+    model:"Corolla",
+    year:2020
+}
+
+let {make ,model,year}=car;
+console.log(`The car was made by: [${make}] and it's model is [${model}]`);
